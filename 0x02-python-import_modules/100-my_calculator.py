85ce891f933c7c62d8bdbd3ee31026f9789d5087
FairTee <stephenodjidja7@gmail.com>
2023-10-05 10:07:09 -0700

    My Calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,6 +4,10 @@ if __name__ == '__main__':
     import sys
     from calculator_1 import add, sub, mul, div
 
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
     def calculate(a, operator, b):
         if operator == '+':
             result = add(a, b)
@@ -23,7 +27,4 @@ if __name__ == '__main__':
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
     calculate(a, operator, b)
