f5298479300961d6a000586aedc56105bb5406f8
FairTee <stephenodjidja7@gmail.com>
2023-10-07 02:35:50 -0700

    Multiple Returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        first = None
+    else:
+        first = sentence[0]
+
+    return length, first
+
+
+if __name__ == "__main__":
+    sentence = "At school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))
