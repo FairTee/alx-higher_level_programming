65cd8384056a2b66e73a0da74d474a9fe9fda97c
FairTee <stephenodjidja7@gmail.com>
2023-10-24 11:34:11 -0700

    SQuare with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""We define a class Square."""
+
+
 class Square:
+    """We represent a square."""
+
     def __init__(self, size):
+        """We initialize a new Square."""
         self.__size = size
