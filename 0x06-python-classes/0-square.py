712e8205e93ec373acdba4804139474b6b35dead
FairTee <stephenodjidja7@gmail.com>
2023-10-24 11:28:04 -0700

    First square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 class Square:
-    """
-    This class defines a basic Square.
+    """This class defines a basic Square."""
 
-    - The 'class Square:' line defines a new class named 'Square'.
-    - The 'pass' statement is a placeholder and does nothing in the class definition.
-    """
+    """The 'class Square:' line defines a new class named 'Square'."""
+    """The 'pass' statement does nothing in the class definition."""
 
     pass
