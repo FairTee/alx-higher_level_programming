aa0c507340c3784b18cb5e3166d2cf3434e7c918
FairTee <stephenodjidja7@gmail.com>
2023-10-28 04:04:55 -0700

    Integers addition/divide a matrix

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This function that adds 2 integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float, optional): The second number. Default is 98.
+
+    Returns:
+    int: The addition of a and b.
+
+    Raises:
+    TypeError: If a or b is not an integer or float.
+    """
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
