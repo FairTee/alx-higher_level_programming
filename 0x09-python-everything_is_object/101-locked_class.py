a95c967c28a34b6094dc4895b9b188a1b0932e97
FairTee <stephenodjidja7@gmail.com>
2023-10-31 11:10:32 -0700

    Objects

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""
+Defines a locked class.
+"""
+
+
 class LockedClass:
+    """
+    The LockedClass restricts the creation of new instance attributes
+    to only allow 'first_name' as an attribute.
+    """
+
     __slots__ = ['first_name']
 
     def __init__(self):
+        """
+        Initializes a new instance of LockedClass.
+        """
         pass
