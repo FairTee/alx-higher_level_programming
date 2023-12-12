cf31a66ef696e0752897d2af6d98333a424e8d71
FairTee <stephenodjidja7@gmail.com>
2023-12-12 04:45:51 -0800

    Removed a database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,5 @@
 -- Script to create the database hbtn_0c_0 if it doesn't exist
 
--- Attempt to create the database; if it already exists, ignore the error
+-- Attempt to create the database;
+-- if it already exists, ignore the error
 CREATE DATABASE IF NOT EXISTS hbtn_Oc_O;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Script to delete the database hbtn_Oc_O if it exists
+
+-- Attempt to drop the database;
+-- if it doesn't exist, ignore the error
+DROP DATABASE IF EXISTS hbtn_Oc_O;
