801cde7ae0bb378d13eb38df47b50cde6dfbb20a
FairTee <stephenodjidja7@gmail.com>
2023-12-12 05:07:25 -0800

    Count 89s

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- Script to display the number of records with id = 89 in the table first_table
+
+-- Query to count the number of records with id = 89
+SELECT COUNT(*) AS count_89
+FROM `first_table`
+WHERE `id` = 89;
