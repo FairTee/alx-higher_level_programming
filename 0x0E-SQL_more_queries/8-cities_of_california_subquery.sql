de1c41047b8e632ee811c72a7dd743460408fbc4
FairTee <stephenodjidja7@gmail.com>
2023-12-13 09:35:55 -0800

    Without genre

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,5 @@
--- 8-cities_of_california_subquery.sql
-
--- Use the hbtn_0d_usa database.
-USE hbtn_0d_usa;
-
 -- Select the cities of California
+
 SELECT id, name
 FROM cities
 WHERE state_id IN (SELECT id FROM states WHERE name = 'California')
