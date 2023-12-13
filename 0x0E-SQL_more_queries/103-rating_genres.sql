231a0974540ada9e6f6323841c20b99335c8d4ef
FairTee <stephenodjidja7@gmail.com>
2023-12-13 10:13:03 -0800

    Genres

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,23 +1,12 @@
--- Script that lists all genres in the database hbtn_0d_tvshows_rate by their rating
+-- Script Description:
+-- List all genres in the database hbtn_0d_tvshows_rate by their rating.
 
-SELECT `name`, SUM(`rate`) AS `rating`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON s.`genre_id` = g.`id`
+-- SELECT statement to retrieve genres and their rating sum.
+SELECT tv_genres.name, SUM(rate) AS rating
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_genres.id
+ORDER BY rating DESC, tv_genres.name;
 
-       INNER JOIN `tv_show_ratings` AS r
-       ON r.`show_id` = s.`show_id`
- GROUP BY `name`
- ORDER BY `rating` DESC;
-
--- Ordered by descending rating.
-
-SELECT `name`, SUM(`rate`) AS `rating`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON s.`genre_id` = g.`id`
-
-       INNER JOIN `tv_show_ratings` AS r
-       ON r.`show_id` = s.`show_id`
- GROUP BY `name`
- ORDER BY `rating` DESC;
