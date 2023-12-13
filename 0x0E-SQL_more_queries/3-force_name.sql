0161b522418e5a3ebe0b457c209c0adce1dc03bc
FairTee <stephenodjidja7@gmail.com>
2023-12-13 07:46:16 -0800

    Creates the table id_not_null on your MySQL server.

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,10 +1,10 @@
--- Script that creates the table force_name on your MySQL server.
-
--- Use the hbtn_0d_2 database
-USE hbtn_0d_2;
-
--- Create table force_name if it doesn't exist
+-- Create the force_name table if it doesn't exist
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
+
+-- Insert data only if it doesn't exist
+INSERT INTO force_name (id, name) VALUES (89, 'Best School') 
+ON DUPLICATE KEY UPDATE name = VALUES(name);
