05022ce483b3b0909fbd43bd240feb6fc3f3f3e9
FairTee <stephenodjidja7@gmail.com>
2024-01-09 05:07:55 -0800

    Object scopes

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
@@ -10,13 +10,13 @@ class Rectangle {
     }
   }
 
-  print() {
-      for (let i = 0; i < this.height; i++) {
-      let s = '';
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let q = '';
       for (let j = 0; j < this.width; j++) {
-        s += 'X';
+        q += 'X';
       }
-      console.log(s);
+      console.log(q);
     }
   }
 }
