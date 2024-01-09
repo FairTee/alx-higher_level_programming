f487ef348e7f1627ea5ca910c2d7ffe437134de0
FairTee <stephenodjidja7@gmail.com>
2024-01-09 05:23:20 -0800

    Server

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  // Create a reversed version of the list without using the built-in reverse method
+  const reversedList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return reversedList;
+};
