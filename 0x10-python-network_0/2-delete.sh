0d0c6dad15c4981688f74ee551861351ed7fa389
FairTee <stephenodjidja7@gmail.com>
2024-01-25 19:55:26 -0800

    Fixed the three line erros

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This one sends a DELETE request to the URL
-# And displays the body of a response
+# This one sends a DELETE request to the URL and displays the body of a response
 curl -sX DELETE "$1"
